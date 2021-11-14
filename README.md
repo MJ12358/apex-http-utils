8dd0587c5ca9f2c8addfee2f18a6fb4fc8868739
MJ12358 <wtpltlf.dev@gmail.com>
2021-11-14 17:07:00 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # apex-http-utils
 Utilities to make Http requests easier.
+
+This is a WIP
---
+++ force-app/main/default/aura/ServerAction/ServerActionHelper.js
@@ -0,0 +1,42 @@
+({
+
+	handleErrors: function(args, errors) {
+		if (args.disableErrorNotification === true) {
+			return;
+		}
+		let isUnknownError = true;
+		if (errors && Array.isArray(errors) && errors.length > 0) {
+			errors.forEach(error => {
+				if (error.message) {
+					this.displayError(error.message, args);
+					isUnknownError = false;
+				}
+				const pageErrors = error.pageErrors;
+				if (pageErrors && Array.isArray(pageErrors) && pageErrors.length > 0) {
+					pageErrors.forEach(pageError => {
+						if (pageError.message) {
+							this.displayError(pageError.message, args);
+							isUnknownError = false;
+						}
+					});
+				}
+			});
+		}
+		if (isUnknownError) {
+			this.displayError('An unknown error occurred', args);
+		}
+	},
+
+	displayError: function(errorMessage, args) {
+		console.error('Server Error: ', errorMessage);
+		console.error('Action: ', args.action.getName(), ' Params: ', JSON.stringify(args.params));
+		const evt = $A.get('e.force:showToast');
+		evt.setParams({
+			message: errorMessage,
+			mode: 'sticky',
+			type: 'error'
+		});
+		evt.fire();
+	}
+		
+})
